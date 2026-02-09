348b73b1b8c096fac760a64d18521286682ca3eb
Alicya <alicyapmarras@gmail.com>
2026-02-09 11:30:56 +0100

    Premier commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animation des liens
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        // Animation Burger
+        burger.classList.toggle('toggle');
+    });
+}
+
+// Appel de la fonction
+navSlide();
